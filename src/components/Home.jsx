93574ef1b43fc16279b3f73b1becccce43532a6d
Rodney Cunanan <36611582+rodmc1@users.noreply.github.com>
2025-03-07 16:38:06 +0800

    initial commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,57 @@
-import React from 'react';
+import { useState } from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
-
+import { TypeAnimation } from 'react-type-animation';
+import img from './../assets/profile2.jpg';
 const Home = () => {
+  const [textColor, setTextColor] = useState('PaleTurquoise');
+
   return (
-    <div name='home' className='w-full h-screen bg-[#0a192f]'>
-      {/* Container */}
-      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p className='text-pink-600'>Hi, my name is</p>
-        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
-          SAI KUMAR
-        </h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
-          I'm a Full Stack Developer.
-        </h2>
-        <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          I’m a full-stack developer specializing in building (and occasionally
-          designing) exceptional digital experiences. Currently, I’m focused on
-          building responsive full-stack web applications.
-        </p>
-        <div>
-          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
-            View Work
-            <span className='group-hover:rotate-90 duration-300'>
-              <HiArrowNarrowRight className='ml-3 ' />
-            </span>
-          </button>
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      <div className="mx-auto px-8 flex justify-center h-full flex flex-row landing-intro">
+        <div className="basis-1/5 profile-img">
+          <img src={img} alt="test" className="rounded-full max-h-64 drop-shadow-lg saturate-50" />
+        </div>
+        <div className="flex-col basis-2/3">
+          <h2 className="text-3xl sm:text-4xl font-bold text-[#ccd6f6]">Rodney Cunanan</h2>
+          <div
+            style={{
+              fontSize: '35px',
+              color: textColor
+            }}>
+            <TypeAnimation
+              sequence={[
+                () => setTextColor('PaleTurquoise'),
+                `I'm a`,
+                700,
+                `I'm a Web Developer`,
+                1000,
+                `Specialized in React JS`,
+                () => setTextColor('DeepSkyBlue'),
+                1000
+              ]}
+              wrapper="span"
+              speed={50}
+              className="text-4xl sm:text-7xl font-bold"
+              repeat={Infinity}
+            />
+          </div>
+          <p className="text-[#8892b0] py-4 max-w-[700px]">
+            A software engineer who specializes in building web applications.
+            <br />
+            {/* Delivering highly executed front-end user experiences by paying close attention to the nuances of design,
+            optimization, and performance.  */}
+            Passionate about creating seamless user experiences through efficient front-end architecture, performance
+            optimization, and modern development best practices. Experienced in backend development and cloud-based
+            solutions to deliver scalable and maintainable applications.
+          </p>
+          <div>
+            <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+              View Work
+              <span className="group-hover:rotate-90 duration-300">
+                <HiArrowNarrowRight className="ml-3 " />
+              </span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/data/data.js
+++ src/data/data.js
@@ -1,35 +1,177 @@
-import WorkIm from '../assets/projects/workImg.jpeg'
-import RealEst from '../assets/projects/realestate.jpg'
+import WorkIm from '../assets/projects/workImg.jpeg';
+import RealEst from '../assets/projects/realestate.jpg';
+import DevSphere from '../assets/projects/ds1.jpg';
+import Aicpa from '../assets/projects/aicpa1.jpg';
+import Wms from '../assets/projects/wms1.png';
+import Delibeery from '../assets/projects/delibeery.png';
 
-export const data=[
-    {
-        id:1,
-        name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
-    },
-    {
-        id:2,
-        name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
-    },
-    {
-        id:3,
-        name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
-    },
-    {
-        id:4,
-        name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
-    },
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+import JS from '../assets/javascript.png';
+import React from '../assets/react.png';
+import NodeJS from '../assets/node.png';
+import Vue from '../assets/vue.png';
+import Tailwind from '../assets/tailwind.png';
+import Sass from '../assets/sass.png';
+import Php from '../assets/php.png';
+import Git from '../assets/github.png';
+import Laravel from '../assets/laravel.png';
+import Material from '../assets/material.png';
+import Mongo from '../assets/mongo.png';
+import Redux from '../assets/redux.png';
+import Firebase from '../assets/firebase.png';
+import Bootstrap from '../assets/bootstrap.png';
 
+export const data = [
+  {
+    id: 1,
+    name: 'Dev Sphere',
+    image: DevSphere,
+    github: 'https://github.com/rodmc1/DevSphere',
+    live: 'https://dev-sphere-rodmc1s-projects.vercel.app/',
+    description:
+      'DevSphere is a modern web application designed to help developers find job opportunities that match their skills and preferences. Built with Vue.js 3, Tailwind CSS, and Vite, it offers a fast and responsive user experience. The backend is powered by JSON Server, deployed on Render, serving as a lightweight REST API for job listings. The application is fully mobile-friendly and follows a serverless architecture for easy scalability'
+  },
+  {
+    id: 2,
+    name: 'AICPA & CIMA',
+    image: Aicpa,
+    github: '',
+    live: 'https://www.aicpa-cima.com/',
+    description:
+      'This website is provided by the Association of International Certified Professional Accountants. As a member of their developer team, I contributed to developing the front end of the application. This project highlights my expertise in front-end development, teamwork, and delivering solutions that align with organizational goals.'
+  },
+  {
+    id: 3,
+    name: 'Warehouse Management System',
+    image: Wms,
+    github: 'https://github.com/rodmc1/WMS',
+    live: 'https://wms.inteluck.com',
+    description:
+      'The Warehouse Management System (WMS) is application designed to streamline inventory tracking, order management, and warehouse operations. Built with modern web technologies, the system provides an intuitive interface for managing stock levels, processing orders, and optimizing warehouse workflows. As the sole front-end developer of this application, I developed the entire user interface using React JS.'
+  },
+  {
+    id: 4,
+    name: 'Delibeery',
+    image: Delibeery,
+    github: '',
+    live: '',
+    description:
+      'My Contribution: As the sole developer of Delibeery, I was responsible for the end-to-end development of the application, including both the front-end and back-end. Delibeery demonstrates my ability to independently conceptualize, develop, and deliver a full-stack application that meets user needs and business requirements.'
+  }
+];
 
-]
+export const skillsData = [
+  {
+    id: 1,
+    name: 'HTML',
+    image: HTML,
+    github: '',
+    live: ''
+  },
+  {
+    id: 2,
+    name: 'CSS',
+    image: CSS,
+    github: '',
+    live: ''
+  },
+  {
+    id: 3,
+    name: 'JavaScript',
+    image: JS,
+    github: '',
+    live: ''
+  },
+  {
+    id: 4,
+    name: 'React',
+    image: React,
+    github: '',
+    live: ''
+  },
+  {
+    id: 5,
+    name: 'NodeJS',
+    image: NodeJS,
+    github: '',
+    live: ''
+  },
+  {
+    id: 6,
+    name: 'Tailwind',
+    image: Tailwind,
+    github: '',
+    live: ''
+  },
+  {
+    id: 7,
+    name: 'Mongo',
+    image: Mongo,
+    github: '',
+    live: ''
+  },
+  {
+    id: 8,
+    name: 'Git',
+    image: Git,
+    github: '',
+    live: ''
+  },
+  {
+    id: 9,
+    name: 'Sass',
+    image: Sass,
+    github: '',
+    live: ''
+  },
+  {
+    id: 10,
+    name: 'Vue',
+    image: Vue,
+    github: '',
+    live: ''
+  },
+  {
+    id: 11,
+    name: 'Material',
+    image: Material,
+    github: '',
+    live: ''
+  },
+  {
+    id: 12,
+    name: 'PHP',
+    image: Php,
+    github: '',
+    live: ''
+  },
+  {
+    id: 13,
+    name: 'Redux',
+    image: Redux,
+    github: '',
+    live: ''
+  },
+  {
+    id: 14,
+    name: 'Laravel',
+    image: Laravel,
+    github: '',
+    live: ''
+  },
+  {
+    id: 15,
+    name: 'Bootstrap',
+    image: Bootstrap,
+    github: '',
+    live: ''
+  },
+  {
+    id: 16,
+    name: 'Firebase',
+    image: Firebase,
+    github: '',
+    live: ''
+  }
+];
